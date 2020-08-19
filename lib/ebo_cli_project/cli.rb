7481aa50a6e2e3750b85ec00e5a43b2d715c9bfc
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2020-08-18 21:38:45 -0700

    edit cli

--- lib/ebo_cli_project/cli.rb
+++ lib/ebo_cli_project/cli.rb
@@ -2,6 +2,7 @@ class EboCliProject::Cli
   
   def greeting
     puts 'yoo, this is my cli project cli'
+    puts 'I just added this line in vs code and I want to make sure this is updated in git once it is pushed...'
   end 
   
 end 
