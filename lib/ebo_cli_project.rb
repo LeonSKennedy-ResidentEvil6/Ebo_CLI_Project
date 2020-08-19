cf9bbc87ef62075f529ccdbafd2cb8e41dc7b7c0
Ebo Lee <yiboli.coding@gmail.com>
2020-08-19 03:28:04 +0000

    initial setup

---
+++ lib/ebo_cli_project.rb
@@ -0,0 +1,6 @@
+require "ebo_cli_project/version"
+
+module EboCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ebo_cli_project/version.rb
@@ -0,0 +1,3 @@
+module EboCliProject
+  VERSION = "0.1.0"
+end
